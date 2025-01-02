09c900288961cf71ab2cfaf72c6c2c79a712bea9
imagineapril <julia.timokhina604@gmail.com>
2025-01-02 23:06:06 +0300

    solve carbon-dating

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,7 +3,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -15,7 +15,6 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function countCats(matrix) {
-  // throw new NotImplementedError('Not implemented');
   let count = 0;
   for (let i = 0; i < matrix.length; i++)  {
     for (let j = 0; j < matrix[i].length; j++)  {
