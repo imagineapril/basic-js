ad05ad7b67957084650f25a77cb7345b2970cb49
Julia Timokhina <juliatimokhina@Julias-MacBook-Pro.local>
2025-10-30 22:45:52 +0300

    feat: add chain and mac

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,20 @@ const { NotImplementedError } = require('../lib');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function isMAC48Address(n) {
+  let macAddress = 0;
+  const regExp = /^[\da-f]+$/i;
+  const arr = n.split('-');
+  if (arr.length !== 6) {
+    return false;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length !== 2) {
+      return false;
+    }
+    if (regExp.test(arr[i])) macAddress += 1;
+  }
+  return macAddress === 6 ? true : false;
 }
 
 module.exports = {
