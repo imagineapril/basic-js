f85413394ad3f433e8fea3e21347bbbf331dbb86
Julia Timokhina <juliatimokhina@Julias-MacBook-Pro.local>
2025-10-30 22:36:17 +0300

    feat: implement transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,28 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+  let transformedArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--double-next':
+        if (arr[i + 1]) transformedArr.push(arr[i + 1]);
+        break;
+      case '--double-prev':
+        if (arr[i - 1]) transformedArr.push(arr[i - 1]);
+        break;
+      case '--discard-next':
+        i += 2;
+        break;
+      case '--discard-prev':
+        transformedArr.pop();
+        break;
+      default:
+        transformedArr.push(arr[i]);
+    }
+  }
+  return transformedArr;
 }
 
 module.exports = {
